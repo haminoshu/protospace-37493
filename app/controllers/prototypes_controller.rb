cd34c5faf298bdf2cf9236f3a1c6012cb6f7f0b5
haminoshu <hamichan1104@gmail.com>
2022-02-23 16:32:14 +0900

    挙動の修正①

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   before_action :set_prototype, only: [:edit, :destroy]
 
   def index
@@ -48,6 +49,9 @@ class PrototypesController < ApplicationController
     prototype = Prototype.find(params[:id])
     prototype.destroy
     redirect_to root_path
+    unless user_signed_in?
+      redirect_to action: :root_path
+    end
   end
 
   private
@@ -57,7 +61,6 @@ class PrototypesController < ApplicationController
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 end
